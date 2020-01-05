6d1bb6ac200c1c9b3c343e15222e6cb73ac19e67
jagjit-arora <55046977+jagjit-arora@users.noreply.github.com>
2020-01-06 01:54:44 +0530

    Add files via upload

---
+++ GRPC test/grpc/src/main/java/com/test/grpc/service/HelloWorldServiceImpl.java
@@ -0,0 +1,34 @@
+package com.test.grpc.service;
+
+import org.lognet.springboot.grpc.GRpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.test.grpc.proto.service.Greeting;
+import com.test.grpc.proto.service.HelloWorldServiceGrpc;
+import com.test.grpc.proto.service.Person;
+
+import io.grpc.stub.StreamObserver;
+
+@GRpcService
+public class HelloWorldServiceImpl
+    extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(HelloWorldServiceImpl.class);
+
+  @Override
+  public void sayHello(Person request,
+      StreamObserver<Greeting> responseObserver) {
+    LOGGER.info("server received {}", request);
+
+    String message = "Hello " + request.getFirstName() + " "
+        + request.getLastName() + "!";
+    Greeting greeting =
+        Greeting.newBuilder().setMessage(message).build();
+    LOGGER.info("server responded {}", greeting);
+
+    responseObserver.onNext(greeting);
+    responseObserver.onCompleted();
+  }
+}
